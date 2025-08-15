3519d23a2b035013afd09c524feffd3dd236bb7f
Matias Perrone <github@matiasperrone.com>
2025-08-15 12:59:31 -0300

    Add needed plugins

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,11 +4,12 @@ import DefaultPagePreview from './preview-templates/DefaultPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
-import { Widget as FileRelationWidget } from '@ncwidgets/file-relation'
-import { Widget as IdWidget } from '@ncwidgets/id'
+// Temporarily commented out due to webpack 5 compatibility issues
+// import { Widget as FileRelationWidget } from '@ncwidgets/file-relation'
+// import { Widget as IdWidget } from '@ncwidgets/id'
 
-CMS.registerWidget(IdWidget)
-CMS.registerWidget(FileRelationWidget)
+// CMS.registerWidget(IdWidget)
+// CMS.registerWidget(FileRelationWidget)
 
 CMS.registerPreviewStyle('style/styles.scss');
 
