4cfcb2db9e33b3892d6368e3e4350c894d4e5c61
Matias Perrone <matias.perrone@exomindset.co>
2025-09-03 14:27:46 -0300

    Add seo image fixes

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -3,51 +3,57 @@ import PropTypes from 'prop-types'
 import { graphql, StaticQuery, Link } from 'gatsby'
 import { kebabCase } from 'lodash'
 
-const BlogRoll = ({ data: { edges: posts }, customFilter }) => (
-  !Array.isArray(posts) || posts.length === 0 ?
-    <div>There don't seem to be any posts that match.</div>
-    :
-    posts.map(({ node: post }) => {
-      if (customFilter) {
-        if (post.frontmatter?.author === customFilter || (post.frontmatter?.category && post.frontmatter?.category[0]?.label === customFilter)) {
-          return (
-            <div className="article-excerpt" metalink="https://www.google.com/" key={post.id}>
-              <h5 className="article-excerpt-title">
-                <a href={post.fields.slug} className="">{post.frontmatter?.title}</a>
-              </h5>
-              <div className="article-excerpt-entry">
-                <div>
-                  <p>{post.excerpt}</p>
+const BlogRoll = ({ data, customFilter }) => {
+  const { edges: posts } = data.allMarkdownRemark;
+
+  return (
+    <>
+      {!Array.isArray(posts) || posts.length === 0 ?
+        <div>There don't seem to be any posts that match.</div>
+        :
+        posts.map(({ node: post }) => {
+          if (customFilter) {
+            if (post.frontmatter?.author === customFilter || (post.frontmatter?.category && post.frontmatter?.category[0]?.label === customFilter)) {
+              return (
+                <div className="article-excerpt" metalink="https://www.google.com/" key={post.id}>
+                  <h5 className="article-excerpt-title">
+                    <a href={post.fields.slug} className="">{post.frontmatter?.title}</a>
+                  </h5>
+                  <div className="article-excerpt-entry">
+                    <div>
+                      <p>{post.excerpt}</p>
+                    </div>
+                  </div>
+                  <div className="article-excerpt-meta">
+                    <p>By <Link to={`/author/${kebabCase(post.frontmatter?.author)}/`}>{post.frontmatter?.author}</Link> on {post.frontmatter?.date}</p>
+                  </div>
+                </div>
+              )
+            } else {
+              return null
+            }
+          } else {
+            return (
+              <div className="article-excerpt" metalink="https://www.google.com/" key={post.id}>
+                <h5 className="article-excerpt-title">
+                  <a href={post.fields.slug} className="">{post.frontmatter?.title}</a>
+                </h5>
+                <div className="article-excerpt-entry">
+                  <div>
+                    <p>{post.excerpt}</p>
+                  </div>
+                </div>
+                <div className="article-excerpt-meta">
+                  <p>By <Link to={`/author/${kebabCase(post.frontmatter?.author)}/`}>{post.frontmatter?.author}</Link> on {post.frontmatter?.date}</p>
                 </div>
               </div>
-              <div className="article-excerpt-meta">
-                <p>By <Link to={`/author/${kebabCase(post.frontmatter?.author)}/`}>{post.frontmatter?.author}</Link> on {post.frontmatter?.date}</p>
-              </div>
-            </div>
-          )
-        } else {
-          return null
+            )
+          }
         }
-      } else {
-        return (
-          <div className="article-excerpt" metalink="https://www.google.com/" key={post.id}>
-            <h5 className="article-excerpt-title">
-              <a href={post.fields.slug} className="">{post.frontmatter?.title}</a>
-            </h5>
-            <div className="article-excerpt-entry">
-              <div>
-                <p>{post.excerpt}</p>
-              </div>
-            </div>
-            <div className="article-excerpt-meta">
-              <p>By <Link to={`/author/${kebabCase(post.frontmatter?.author)}/`}>{post.frontmatter?.author}</Link> on {post.frontmatter?.date}</p>
-            </div>
-          </div>
-        )
-      }
-    }
-    )
-)
+        )}
+    </>
+  )
+}
 
 BlogRoll.propTypes = {
   data: PropTypes.shape({
